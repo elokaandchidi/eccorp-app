b1568a1eba18fb04bf7b56d6767f7f3568f83a39
Ajaloleru Samson <37838638+samajaloleru@users.noreply.github.com>
2025-03-19 22:30:20 +0100

    updated query to fetch professionals and posts

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
   }, [pathname]);
   
   return (
-    <div className="App ">
+    <div className="flex flex-col items-center w-full overflow-auto h-screen">
       <Routes>
         <Route path="/*" element={<IndexRoutes/>} />
       </Routes>
